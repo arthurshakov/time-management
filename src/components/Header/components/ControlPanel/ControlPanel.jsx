ca3775476c348196b91da1e9111398e9a2724cc0
arthurshakov <33399638+arthurshakov@users.noreply.github.com>
2025-06-13 21:33:46 +0300

    added mobile styles for the header

--- src/components/Header/components/ControlPanel/ControlPanel.jsx
+++ src/components/Header/components/ControlPanel/ControlPanel.jsx
@@ -4,9 +4,11 @@ import styles from './control-panel.module.scss';
 export const ControlPanel = () => {
   return (
     <div className={styles['control-panel']}>
-      <Link to="/login" className={styles['text-link']}>Login</Link>
-      <span>&nbsp;|&nbsp;</span>
-      <Link to="/login" className={styles['text-link']}>Authorize</Link>
+      <div>
+        <Link to="/login" className={styles['text-link']}>Log in</Link>
+        <span>&nbsp;|&nbsp;</span>
+        <Link to="/login" className={styles['text-link']}>Sign up</Link>
+      </div>
 
       <Link to="/dashboard" className={styles['icon-link']}>
         <span class="fa-stack fa-lg">
